91a6cf467d906f6d6906da2a4e551ba5142fc664
Lao-Qi <2231675321@qq.com>
2022-07-11 14:38:01 +0800

    完善搜索用户好友列表接口

--- Route/Search/FriendsList.js
+++ Route/Search/FriendsList.js
@@ -4,39 +4,50 @@ const { Users } = require("../../model/model")
 const router = require("express").Router()
 
 router.get("/FriendsList", VerifyToken, FindTokenIDUser, async (req, res) => {
-    const friendAccountList = req.doc.friends.map((friend) => friend.account)
-    const UserList = await findUsers(friendAccountList)
-    res.send({
-        code: 200,
-        friendsList: req.doc.friends,
-        userList: UserList ?? [],
-        msg: "获取成功",
-    })
+    const friendAccountList = req.doc.friends.map(friend => friend.account)
+    findUsers(friendAccountList)
+        .then(docs => {
+            res.send({
+                code: 200,
+                friendsList: req.doc.friends,
+                userList: docs,
+                msg: "获取成功"
+            })
+        })
+        .catch(err => {
+            console.error(err)
+            res.send({
+                code: 200,
+                friendList: req.doc.friend,
+                userList: [],
+                msg: "数据库查询失败"
+            })
+        })
 })
 
 async function findUsers(AccountList) {
-    return new Promise((res) => {
+    return new Promise((res, rej) => {
         Users.find(
             {
                 $and: [
                     {
                         account: {
-                            $in: AccountList,
-                        },
+                            $in: AccountList
+                        }
                     },
                     {
-                        del: false,
-                    },
-                ],
+                        del: false
+                    }
+                ]
             },
             {
                 _id: 1,
                 name: 1,
                 account: 1,
-                avatar: 1,
+                avatar: 1
             },
             (err, docs) => {
-                res(docs)
+                err ? rej(err) : res(docs)
             }
         )
     })
