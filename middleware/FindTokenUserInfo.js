36ee47460317638fd361be5e49362ceb282437a9
Lao-Qi <2231675321@qq.com>
2022-07-15 19:54:09 +0800

    添加一个通过token查询用户详细信息的中间件

---
+++ middleware/FindTokenUserInfo.js
@@ -0,0 +1,50 @@
+const { UserDetailed } = require("../model/model")
+
+module.exports = async function (req, res, next) {
+    if (req.tokenData.account && req.tokenData.id) {
+        FindUserDetailed(req.tokenData)
+            .then(doc => {
+                if (doc) {
+                    req.userinfo = doc
+                    next()
+                } else {
+                    res.send({
+                        code: 404,
+                        msg: "用户不存在"
+                    })
+                }
+            })
+            .catch(err => {
+                res.send({
+                    code: 500,
+                    msg: "用户查询失败"
+                })
+                console.error(err)
+            })
+    } else {
+        res.send({
+            code: 404,
+            msg: "用户信息不齐全"
+        })
+    }
+}
+
+function FindUserDetailed({ account, id }) {
+    return new Promise((res, rej) => {
+        UserDetailed.findOne(
+            {
+                $and: [
+                    {
+                        account
+                    },
+                    {
+                        id
+                    }
+                ]
+            },
+            (err, doc) => {
+                err ? rej(err) : res(doc)
+            }
+        )
+    })
+}
