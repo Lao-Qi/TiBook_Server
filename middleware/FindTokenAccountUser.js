8fd0cbc7855dc6b4c71fd71fe3127296aeaa80fa
Lao-Qi <2231675321@qq.com>
2022-07-15 19:53:34 +0800

    修改通过token查找用户的中间件的逻辑

--- middleware/FindTokenAccountUser.js
+++ middleware/FindTokenAccountUser.js
@@ -1,36 +1,49 @@
 const { Users } = require("../model/model")
 
 module.exports = async function FindTokenIDUser(req, res, next) {
-    FindUser(req.tokenData.account)
-        .then((doc) => {
-            req.doc = doc
-            next()
-        })
-        .catch((err) => {
-            if (err) {
+    const account = req.tokenData.account ?? req.body.account
+    if (account) {
+        FindUser(req.tokenData.account)
+            .then(doc => {
+                if (doc) {
+                    req.doc = doc
+                    next()
+                } else {
+                    res.send({
+                        code: 400,
+                        data: {
+                            account
+                        },
+                        msg: "用户不存在"
+                    })
+                }
+            })
+            .catch(err => {
                 res.send({
                     code: 500,
-                    msg: "查询失败",
+                    data: {
+                        account
+                    },
+                    msg: "查询失败"
                 })
                 console.error(err)
-            } else {
-                res.send({
-                    code: 404,
-                    account: req.tokenData.account,
-                    msg: "认证的用户不存在",
-                })
-            }
+            })
+    } else {
+        res.send({
+            code: 404,
+            msg: "账户为空"
         })
+    }
 }
 
 async function FindUser(account) {
     return new Promise((res, rej) => {
         Users.findOne(
             {
-                account,
+                account
             },
             (err, doc) => {
-                err || !doc ? rej(err) : res(doc)
+                err ? rej(err) : res(doc)
             }
         )
     })
