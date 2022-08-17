022071580750adc7b2ed5099953d611b917916cb
Lao-Qi <2231675321@qq.com>
2022-08-17 21:40:10 +0800

    添加上传个性背景图片接口

--- Route/User.js
+++ Route/User.js
@@ -14,9 +14,12 @@ const register = require("./User/register")
 const reomve = require("./User/remove")
 // 添加好友
 const addFriend = require("./User/addFriend")
+// 上传个性背景图片
+const uploadPPictures = require("./User/uploadPPictures")
 
 // 注册路由
 router.use(uploadAvatar) // post /uploadAvatar
+router.use(uploadPPictures) // post /uploadPPictures
 router.use(login) // post /login
 router.use(register) // post /register
 router.use(addFriend) // post /addFriend
