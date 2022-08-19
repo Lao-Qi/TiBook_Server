8ee97be20ddc9e23578f943f2198d289d32667d7
Lao-Qi <2231675321@qq.com>
2022-08-20 01:15:51 +0800

    修改获取token用户基本数据接口逻辑

--- Route/Search/FindTokenUser.js
+++ Route/Search/FindTokenUser.js
@@ -5,17 +5,18 @@
  * 这个接口主要是用于替代前端后台的用户基本信息本地存储，让用户的基本信息和服务器保持一致
  */
 const verifyToken = require("../../middleware/verify-token")
+const FindTokenUser = require("../../middleware/FindTokenAccountUser")
 const { setAvatarURL } = require("../../lib/SmallFunctionIntegration")
 const router = require("express").Router()
 
-router.get("/findTokenUser", verifyToken, (req, res) => {
+router.get("/findTokenUser", verifyToken, FindTokenUser, (req, res) => {
     res.send({
         code: 200,
         data: {
-            account: req.tokenData.account,
-            avatar: setAvatarURL(req.tokenData.avatar),
-            name: req.tokenData.name,
-            id: req.tokenData.id
+            account: req.doc.account,
+            avatar: setAvatarURL(req.doc.avatar),
+            name: req.doc.name,
+            id: req.doc.id
         },
         msg: "获取成功"
     })
